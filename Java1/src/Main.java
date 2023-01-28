d0fc66d5361a8de06acbbb81026fca77dc245d74
Nadezhda26m <nadezhda-26m@mail.ru>
2023-01-28 17:23:55 +0300

    add time tracking

--- Java1/src/Main.java
+++ Java1/src/Main.java
@@ -1,3 +1,4 @@
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class Main {
@@ -5,6 +6,17 @@ public class Main {
         Scanner iScanner = new Scanner(System.in);
         System.out.print("Введите ваше имя: ");
         String name = iScanner.nextLine();
-        System.out.printf("Привет, %s!", name);
+
+        int hour = LocalTime.now().getHour();
+
+        if (hour >= 5 && hour < 12) {
+            System.out.printf("Доброе утро, %s!", name);
+        } else if (hour >= 12 && hour < 18) {
+            System.out.printf("Добрый день, %s!", name);
+        } else if (hour >= 18 && hour < 23) {
+            System.out.printf("Добрый вечер, %s!", name);
+        } else {
+            System.out.printf("Доброй ночи, %s!", name);
+        }
     }
 }
