9530fd0c03f971862496cdcd44e73ec3b81c8e9d
Nadezhda26m <nadezhda-26m@mail.ru>
2023-02-20 16:39:02 +0300

    add reverseLinkedList() method

--- Java1/src/MainReverse.java
+++ Java1/src/MainReverse.java
@@ -15,4 +15,13 @@ public class MainReverse {
         System.out.println(myList);
         // [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
     }
+
+    public static void reverseLinkedList(LinkedList<Integer> list) {
+        LinkedList<Integer> helpList = new LinkedList<>();
+        int size = list.size();
+        for (int i = 0; i < size; i++) {
+            helpList.addFirst(list.removeFirst());
+        }
+        list.addAll(helpList);
+    }
 }
