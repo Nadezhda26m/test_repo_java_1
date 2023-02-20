1ce03302cf8ba418a3bb68c6f8a2f1eeafe8c6f3
Nadezhda26m <nadezhda-26m@mail.ru>
2023-02-20 20:26:56 +0300

    try revert 3

--- Java1/src/MainReverse.java
+++ Java1/src/MainReverse.java
@@ -1,4 +1,3 @@
-import java.util.Collections;
 import java.util.LinkedList;
 
 public class MainReverse {
@@ -10,23 +9,5 @@ public class MainReverse {
         System.out.println(myList);
         // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 
-        // Вариант 1
-        Collections.reverse(myList);
-        System.out.println(myList);
-        // [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-
-        // Вариант 2
-        reverseLinkedList(myList);
-        System.out.println(myList);
-        // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-    }
-
-    public static void reverseLinkedList(LinkedList<Integer> list) {
-        LinkedList<Integer> helpList = new LinkedList<>();
-        int size = list.size();
-        for (int i = 0; i < size; i++) {
-            helpList.addFirst(list.removeFirst());
-        }
-        list.addAll(helpList);
     }
 }
