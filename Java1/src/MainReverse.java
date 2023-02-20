4d55752c4f841343f5b30b72c07477bbbb33c095
Nadezhda26m <nadezhda-26m@mail.ru>
2023-02-20 16:32:11 +0300

    fill LinkedList

--- Java1/src/MainReverse.java
+++ Java1/src/MainReverse.java
@@ -3,5 +3,10 @@ import java.util.LinkedList;
 public class MainReverse {
     public static void main(String[] args) {
         LinkedList<Integer> myList = new LinkedList<>();
+        for (int i = 0; i < 15; i++) {
+            myList.add(i + 1);
+        }
+        System.out.println(myList);
+        // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
     }
 }
