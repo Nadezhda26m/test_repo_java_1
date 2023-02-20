90a4d20ea6b2bc61ff47e7333cffa8e4b7adeb8c
Nadezhda26m <nadezhda-26m@mail.ru>
2023-02-20 16:38:11 +0300

    use built-in method reverse

--- Java1/src/MainReverse.java
+++ Java1/src/MainReverse.java
@@ -1,3 +1,4 @@
+import java.util.Collections;
 import java.util.LinkedList;
 
 public class MainReverse {
@@ -8,5 +9,10 @@ public class MainReverse {
         }
         System.out.println(myList);
         // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+        // Вариант 1
+        Collections.reverse(myList);
+        System.out.println(myList);
+        // [15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
     }
 }
